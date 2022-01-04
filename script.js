71dde5918dc194d1b4d09746462da8ef334a4c9c
joaquin <jecorrea@protonmail.com>
2022-01-04 17:46:54 -0500

    answer with one pointer and a for loop

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+// function isValidSubsequence(array, sequence) {
+//   // Write your code here.
+//   // O(n) time | O(1) space
+//   let arrIdx = 0;
+//   let seqIdx = 0;
+//   while (arrIdx < array.length && seqIdx < sequence.length) {
+//     if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+//     arrIdx++;
+//   }
+//   return seqIdx === sequence.length;
+// }
+
 function isValidSubsequence(array, sequence) {
   // Write your code here.
-  // O(n) time | O(1) space
-  let arrIdx = 0;
+  // O(n) time | O(1) Space
   let seqIdx = 0;
-  while (arrIdx < array.length && seqIdx < sequence.length) {
-    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
-    arrIdx++;
+  for (const value of array) {
+    if (seqIdx === sequence.length) break;
+    if (sequence[seqIdx] === value) seqIdx++;
   }
   return seqIdx === sequence.length;
 }
