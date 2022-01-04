9e4070a039fb6ecd86f7eba454edfbcb70400e95
joaquin <jecorrea@protonmail.com>
2022-01-04 17:45:17 -0500

    long ans

---
+++ script.js
@@ -0,0 +1,11 @@
+function isValidSubsequence(array, sequence) {
+  // Write your code here.
+  // O(n) time | O(1) space
+  let arrIdx = 0;
+  let seqIdx = 0;
+  while (arrIdx < array.length && seqIdx < sequence.length) {
+    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+    arrIdx++;
+  }
+  return seqIdx === sequence.length;
+}
